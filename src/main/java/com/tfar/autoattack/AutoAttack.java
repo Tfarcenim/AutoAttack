8322102ccfac8e57aad429a1bf94b330f62848c2
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-03-13 17:57:10 -0400

    ree

--- src/main/java/com/tfar/autoattack/AutoAttack.java
+++ src/main/java/com/tfar/autoattack/AutoAttack.java
@@ -1,40 +1,44 @@
 package com.tfar.autoattack;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.EnumHand;
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraftforge.common.config.Config;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
+import static net.minecraftforge.common.MinecraftForge.EVENT_BUS;
 
 @Mod(modid = AutoAttack.MODID, name = AutoAttack.NAME, version = "@VERSION@",clientSideOnly = true)
-@Mod.EventBusSubscriber(value = Side.CLIENT, modid = AutoAttack.MODID)
-@Config(modid = AutoAttack.MODID)
 public class AutoAttack {
 
-    @Config.Ignore
     static final String MODID = "autoattack";
-    @Config.Ignore
     static final String NAME = "Auto Attack";
-    @Config.Ignore
     private static final Minecraft mc = Minecraft.getMinecraft();
-    @Config.Ignore
     private static int tick = 0;
 
-    @Config.Name("attack_empty")
-    public static boolean attack_empty = false;
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e){
+        EVENT_BUS.register(this);
+    }
 
     @SubscribeEvent
-    public static void onRenderTick(TickEvent.ClientTickEvent event) {
+    public void mouse(InputEvent.KeyInputEvent e){
         if (mc.gameSettings.keyBindAttack.isKeyDown() && mc.player != null
-                && mc.player.getCooledAttackStrength(0) >= 1){
-            if (mc.objectMouseOver.entityHit == null && !attack_empty){
-                tick = 0; return;
+                && mc.player.getCooledAttackStrength(0) < 1) {
+            if (mc.objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY) {
+                KeyBinding.setKeyBindState(Minecraft.getMinecraft().gameSettings.keyBindAttack.getKeyCode(), false);
             }
-            if (mc.objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY || attack_empty) {
+        }
+    }
+
+    @SubscribeEvent
+    public void onRenderTick(TickEvent.ClientTickEvent event) {
+        if (mc.gameSettings.keyBindAttack.isKeyDown() && mc.player != null
+                && mc.player.getCooledAttackStrength(0) >= 1) {
+            if (mc.objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY) {
                 if (tick < 1)tick++;
                 else {
                     mc.clickMouse();
